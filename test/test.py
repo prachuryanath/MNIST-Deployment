d52ee0fb22ae4f15bf70e7213e51a515a4eddc3c
Prachurya Nath <prachuryanath00@gmail.com>
2021-07-15 21:56:40 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 import requests
 
-resp = requests.post("http://127.0.0.1:5000/predict", files= {'file':open('download.png', 'rb')})
+resp = requests.post("https://flask-mnist-pal.herokuapp.com/predict", files= {'file':open('download.png', 'rb')})
 
 print(resp.text)
